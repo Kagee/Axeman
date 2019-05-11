7c1cfc4fbdd847cdab313bed7c283b8dc83cc5e2
Anders Einar Hilden <hildenae@gmail.com>
2019-05-12 01:39:07 +0200

    only list restart commands for interresting logs

--- axeman/metadata-check.sh
+++ axeman/metadata-check.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
-find ./output -name '*metadata*' -exec bash -c 'python3.6 simple.py -n -c "$(dirname {})"' \; 2>&1 | grep -E 'length: |Last log update: |To continue: '
+#find ./output -name '*metadata*' -exec bash -c 'python3.6 simple.py -n -c "$(dirname {}); echo "";"' \; 2>&1 | grep -E 'length: |Last log update: |To continue: '
+./simple.py -a | sort -g > estimates.txt; reset; cat estimates.txt
+cat estimates.txt | grep -v '(J)' | awk '/^[1-9][0-9]*\t[1-9]{4}/{ print $4 }' | xargs -L 1 -- ./simple.py -n -c 2>&1 | grep -E "length:|Last log|To continue"
