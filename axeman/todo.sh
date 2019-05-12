7b134d5f57b3ad704eca23e129df944de195c73a
Anders Einar Hilden <hildenae@gmail.com>
2019-05-12 02:45:58 +0200

    Polish script to print todo-jobs

--- axeman/todo.sh
+++ axeman/todo.sh
@@ -1,4 +1,20 @@
-cat foo2 | grep -v '(J)' | grep -P '^[^0]\d{1,5}\t' | awk '{print $4}'  | xargs -L 1 ./simple.py -c 2>&1 | grep 'To continue' > todo.txt
-sed -i 's/-u/-x -n -u/' todo.txt
-sed -i 's/^.*: //' todo.txt
-bash todo.txt
+#!/bin/bash
+./simple.py -b | tee status.txt
+echo "[INFO] status.txt generated"
+cat status.txt
+echo ""
+grep -v ' !OK ' status.txt | while read LINE; do
+  LOG="$(echo "$LINE" | cut -d' ' -f 1)"
+  echo "$LINE" | grep 'missing:' | sed -e "s/\(missing:[^ ]*\)/\n\1\n/g" | grep missing: | \
+    while read MISSING; do 
+      START="$(echo "$MISSING" | cut -d: -f2)"; 
+      END="$(echo "$START" | cut -d- -f2)"; 
+      START="$(echo "$START" | cut -d- -f1)"; 
+      echo "./simple.py -n -u $LOG -s $START -e $END"; 
+    done;
+  END="$(echo "$LINE" | grep 'missing (end)' | sed -e 's/.*missing (end):\([0-9]*\)-\([0-9]*\).*/\1/';)"
+  if [ ! -z "$END" ]; then
+    END="$(echo "$END - 100" | bc)"
+    echo "./simple.py -n -u $LOG -s $END"
+  fi
+done;
