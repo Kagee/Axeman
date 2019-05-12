d680ae657cddd0065335e33a3debf2b0b1183051
Anders Einar Hilden <hildenae@gmail.com>
2019-05-12 04:20:53 +0200

    Print diff on end-missing logs, and skip JIMI-logs

--- axeman/todo.sh
+++ axeman/todo.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-echo "[INFO] Generating status.txt"
-./simple.py -b | tee status.txt | grep -v ' OK '
+if [ "x--fast" != "x$1" ]; then
+  echo "[INFO] Generating status.txt"
+  ./simple.py -b | tee status.txt | grep -v -E ' OK '
+else
+ cat status.txt
+fi
 echo ""
 echo "[INFO] TODOs:"
-grep -v ' OK ' status.txt | while read LINE; do
+grep -v -E ' OK |(J)' status.txt | while read LINE; do
   LOG="$(echo "$LINE" | cut -d' ' -f 1)"
   echo "$LINE" | grep 'missing:' | sed -e "s/\(missing:[^ ]*\)/\n\1\n/g" | grep missing: | \
     while read MISSING; do 
@@ -13,8 +17,9 @@ grep -v ' OK ' status.txt | while read LINE; do
       echo "./simple.py -n -u $LOG -s $START -e $END"; 
     done;
   END="$(echo "$LINE" | grep 'missing (end)' | sed -e 's/.*missing (end):\([0-9]*\)-\([0-9]*\).*/\1/';)"
+  DIFF="$(echo "$LINE" | grep 'missing (end)' | sed -e 's/.*missing (end):[0-9]*-[0-9]* (\([0-9,]*\)).*/\1/';)"
   if [ ! -z "$END" ]; then
     END="$(echo "$END - 100" | bc)"
-    echo "./simple.py -n -u $LOG -s $END"
+    echo "./simple.py -n -u $LOG -s $END # diff: $DIFF"
   fi
 done;
