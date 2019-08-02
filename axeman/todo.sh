48a42e4b4f53b2889aae5f16e3d68ac319e52b1d
Anders Einar Hilden <hildenae@gmail.com>
2019-08-02 13:18:22 +0200

    Write a smarter todo.sh to prevent useless updates to often

--- axeman/todo.sh
+++ axeman/todo.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
-if [ "x--fast" != "x$1" ]; then
-  echo "[INFO] Generating status.txt"
-  ./simple.py -b | tee status.txt | grep -v -E ' OK '
-else
- cat status.txt
-fi
+function update-status() {
+  ./simple.py -b | tee status.txt.tmp | grep -v -E ' OK '
+  mv status.txt.tmp status.txt
+}
+MAXAGE=360
+case "$1" in
+    "")
+        if [[ $(find "status.txt" -cmin +$MAXAGE -print) ]] || [ ! -f "status.txt" ]; then
+          1>&2 echo "[INFO] status.txt older than $MAXAGE minutes, updating ...";
+          update-status
+        else
+          1>&2 echo "[INFO] status.txt younger than $MAXAGE minutes, re-using";
+        fi;;
+    "--no-update"|"--fast")
+        1>&2 echo "[INFO] Forcing re-use of status.txt";;
+    "--update")
+        1>&2 echo "[INFO] Forcing update of status.txt";
+        update-status;;
+    *)
+        1>&2 echo "[ERROR] Unknown option $1";
+        exit 1;;
+esac
 echo ""
 echo "[INFO] TODOs:"
-grep -v -E ' OK ' status.txt | while read LINE; do
+grep -v -E ' OK ' status.txt | while read -r LINE; do
   LOG="$(echo "$LINE" | cut -d' ' -f 1)"
   echo "$LINE" | grep 'missing:' | sed -e "s/\(missing:[^ ]*\)/\n\1\n/g" | grep missing: | \
-    while read MISSING; do 
-      START="$(echo "$MISSING" | cut -d: -f2)"; 
-      END="$(echo "$START" | cut -d- -f2)"; 
-      START="$(echo "$START" | cut -d- -f1)"; 
-      echo "./simple.py -n -u $LOG -s $START -e $END"; 
+    while read -r MISSING; do
+      START="$(echo "$MISSING" | cut -d: -f2)";
+      END="$(echo "$START" | cut -d- -f2)";
+      START="$(echo "$START" | cut -d- -f1)";
+      echo "./simple.py -n -u $LOG -s $START -e $END # $(echo "$END - $START" |bc)";
     done;
   END="$(echo "$LINE" | grep 'missing (end)' | sed -e 's/.*missing (end):\([0-9]*\)-\([0-9]*\).*/\1/';)"
   DIFF="$(echo "$LINE" | grep 'missing (end)' | sed -e 's/.*missing (end):[0-9]*-[0-9]* (\([0-9,]*\)).*/\1/';)"
